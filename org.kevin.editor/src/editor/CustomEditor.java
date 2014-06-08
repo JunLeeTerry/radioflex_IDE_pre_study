78e1c1bf91dc2aa264efad8cc657cc0a6022be84
qzw <qzw@ubuntu.ubuntu-domain>
2014-06-08 13:58:03 +0800

    啊哈哈^_^

---
+++ org.kevin.editor/src/editor/CustomEditor.java
@@ -0,0 +1,12 @@
+package editor;
+
+import org.eclipse.ui.editors.text.TextEditor;
+
+public class CustomEditor extends TextEditor {
+	public static final String ID = "org.kevin.editor.editor";
+	
+	public CustomEditor() {
+		super();
+	}
+	
+}
